35e91e8a24821cd2b96902589c2ad0820db400f3
Carlos <carlos1.teste2024@gmail.com>
2025-01-13 18:40:40 -0300

    Atividade 6

--- CAP 6/7.js
+++ CAP 6/7.js
@@ -1,3 +1,21 @@
+let globalVar = "Eu sou global!";
 
+function minhaFuncao(){
+
+    let localVar = "Eu sou local!";
+console.log("Dentro da função:");
+console.log(globalVar);
+console.log(localVar);
+}
+
+minhaFuncao();
+
+console.log("\nFora da função:");
+console.log(globalVar);
+try {
+    console.log(localVar);
+}catch (error) {
+    console.log(`Erro: ${error.message}`);
+}
 
 
